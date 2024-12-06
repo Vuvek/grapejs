a6cec50c74a18e31cdca0ce640aef9fbb00bb9a9
Vivek Kumar <vivekk@kaushalaminc.com>
2024-12-06 18:46:00 +0530

    env removed

--- src/utils/mongoDb.ts
+++ src/utils/mongoDb.ts
@@ -4,7 +4,7 @@ let db: Db | null = null;
 
 export async function connectToDatabase() {
   if (!client) {
-    client = new MongoClient(process.env.NEXT_PUBLIC_MONGO_URI || "", {});
+    client = new MongoClient(`mongodb+srv://client:client@cluster0.h4wbx.mongodb.net/undefined?retryWrites=true&w=majority&appName=Redefine`, {});
     await client.connect();
   }
   if (!db) {
